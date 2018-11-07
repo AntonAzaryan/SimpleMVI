a58dfa82a5aae2e17da7cae9ed760ace50c8a8e1
Anton Azaryan <anton.azaryan@jetruby.com>
2018-11-07 23:46:28 +0300

    Add EventHandler/StateReducer type aliases Change BasePresenter props visibility modifiers

--- simple-mvi/src/main/java/com/aza/simple/mvi/Presenters.kt
+++ simple-mvi/src/main/java/com/aza/simple/mvi/Presenters.kt
@@ -16,26 +16,29 @@ interface Presenter<VS> {
   val stateProducer: LiveData<VS>
 }
 
+typealias EventHandler = (Observable<Any>) -> Observable<Any>
+typealias StateReducer<VS> = (VS, Any) -> VS
+
 abstract class BasePresenter<VS>(
   initState: () -> VS,
-  eventsHandler: Observable<Any>.() -> Observable<Any>,
-  stateReducer: BiFunction<VS, Any, VS>
+  eventsHandler: EventHandler,
+  stateReducer: StateReducer<VS>
 ) : ViewModel(), Presenter<VS> {
 
-  private val disposables: CompositeDisposable = CompositeDisposable()
+  protected val disposables: CompositeDisposable = CompositeDisposable()
 
-  private val eventRelay: Relay<Any> = PublishRelay.create<Any>().toSerialized()
-  private val stateLiveData: MutableLiveData<VS> = MutableLiveData<VS>().apply { value = initState() }
+  protected val eventRelay: Relay<Any> = PublishRelay.create<Any>().toSerialized()
+  protected val stateLiveData: MutableLiveData<VS> = MutableLiveData<VS>().apply { value = initState() }
+  protected val intentStream: Observable<Any> = eventsHandler(eventRelay).share()
 
   override val eventsConsumer: Consumer<Any> = eventRelay
   override val stateProducer: LiveData<VS> = stateLiveData
 
   init {
-    val intentStream = eventRelay.eventsHandler().share()
     disposables.addAll(
       intentStream.subscribe(eventRelay), // for side effects
       intentStream
-        .scan(stateLiveData.value, stateReducer)
+        .scan(stateLiveData.value, BiFunction(stateReducer))
         .distinctUntilChanged()
         .subscribe { stateLiveData.postValue(it) }
     )
